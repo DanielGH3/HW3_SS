7dae3e34d35d04c687b65f21f4206eb98f1ba5eb
DanielGH3 <118988238+DanielGH3@users.noreply.github.com>
2023-01-08 05:47:41 +0200

    Add files via upload

---
+++ txtfind/txtfind/main.c
@@ -0,0 +1,20 @@
+#include "txtfind.h"
+
+char word[WORD];
+char mode[WORD];
+char empty[LINE];
+
+int main(){
+    getword(word);
+    getword(mode);
+    getuserline(empty);
+
+    if(*mode == 'b'){
+        print_similar_words(word);
+    }else if(*mode == 'a'){
+        print_lines(word);
+    }
+    
+    return 0;
+}
+
